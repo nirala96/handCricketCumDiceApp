32d84892c56a28ce287eb4eb3bb938a243e0e8bf
= <=>
2020-05-06 10:05:09 +0530

    My first android app

---
+++ app/src/main/java/Arunoday/kumar/firstproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package Arunoday.kumar.firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClicked(view: View) {
+        val randomNumber=(1..6).random()
+        textView.text=randomNumber.toString()
+    }
+}
